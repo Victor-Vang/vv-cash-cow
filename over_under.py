f5303ef64c1a36e38936c777bef3e2789bd4f7d8
Victor-Vang <vv.fitness614@gmail.com>
2023-05-23 16:20:10 -0400

    Reorganized code

--- over_under.py
+++ over_under.py
@@ -3,50 +3,53 @@ import inquirer
 import project_utils
 
 
+class SportSelector:
+    def __init__(self):
+        self.sport = None
+
+    def select_sport(self):
+        parser = argparse.ArgumentParser(description="Over/Under Analysis Tool")
+        parser.add_argument("--sport", choices=["NBA", "NCAAF", "NFL", "WNBA",], help="Select a sport: NBA, NCAAF NFL, WNBA")
+        args = parser.parse_args()
+        self.sport = args.sport
+
+        if not self.sport:
+            sports_options = [inquirer.List("sport", message="Please select a sport", choices=["NBA", "NCAAF", "NFL", "WNBA"]),]
+            answers = inquirer.prompt(sports_options)
+            self.sport = answers["sport"]
+        
+        return self.sport
+
+
 class BettingMatchup: 
     def __init__(self, sport: str):
         self.sport = sport.lower()
         self.data = None
 
     def load_data(self):
+        if self.sport is not "WNBA":
+            raise ValueError("This sport has yet to be implimented! Patience!")
         self.data = project_utils.loadYamlAsObject(f"{self.sport}.yaml")
         
         return self.data
 
     def select_team(self):
-        # team_choices = [inquirer.Choice(name=team.name["name"], value=abbreviation) for abbreviation, team in self.data.teams]
-
-        # team_choices = [team_data["name"] for team_data in self.data.teams_iterator()]
-
-        # team_choices = [{"name": team_data["name"], "value": abbreviation} for abbreviation, team_data in self.data.teams.items()]
         team_choices = [team.name for team in self.data.teams.__dict__.values()]
-
         print(team_choices)
+        print(type(team_choices))
         questions = [inquirer.List("team", message="Please select a team", choices=team_choices)]
         
         team_choice = inquirer.prompt(questions)
         
         return team_choice.get("team")
 
-# [!!!]
+if __name__ == "__main__":
+    selector = SportSelector()
+    sport = selector.select_sport()
+    print(sport)
 
-parser = argparse.ArgumentParser(description="Over/Under Analysis Tool")
-parser.add_argument("--sport", choices=["NBA", "NCAAF", "NFL", "WNBA",], help="Select a sport: NBA, NCAAF NFL, WNBA")
-args = parser.parse_args()
-sport_selection = args.sport
-
-if not sport_selection:
-    sports_options = [inquirer.List("sport", message="Please select a sport", choices=["NBA", "NCAAF", "NFL", "WNBA"]),]
-
-    answers = inquirer.prompt(sports_options)
-    sport_selection = answers["sport"]
-
-sport = ""
-
-if sport_selection == "WNBA":
-    sport = "WNBA"
-
-print(sport)
-bet = BettingMatchup(sport)
-data = bet.load_data()
-bet.select_team()
+    bet = BettingMatchup(sport)
+    bet.load_data()
+    chosen_team = bet.select_team()
+    print(chosen_team)
+    print(type(chosen_team))
--- project_utils.py
+++ project_utils.py
@@ -1,25 +1,10 @@
 import json
 import yaml
 
+
 class YamlConfig:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-
-    # def teams_iterator(self):
-    #     for team_abbreviation, team_data in self.__dict__.items():
-    #         if team_abbreviation != "teams":
-    #             yield team_abbreviation, team_data
-
-    def teams_iterator(self):
-        teams = {}
-        for abbreviation, team_data in self.teams.items():
-            team_data[abbreviation] = team_data
-        return teams
-
-# def loadYamlAsObject(file_name: str):
-#     with open(file_name, "r") as stream:
-#         loaded = yaml.safe_load(stream)
-#         return YamlConfig(**loaded)
 
 def loadYamlAsObject(file_name: str):
     with open(file_name, "r") as stream:
